55d37ad62423a77ce187689ed2e9cffe6ae1f8c6
Tori Sanford <tori.danielle.clark91@gmail.com>
2021-01-04 19:45:13 -0500

    committing JS first pass

--- java.js
+++ java.js
@@ -5,10 +5,12 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
+
+generateBtn.addEventListener("click", e => { 
+    console.log (e.target)
+})
