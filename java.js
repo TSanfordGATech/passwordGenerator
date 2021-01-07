9839ba46c3c6cb0350fb2cb1e8cf75e719153dd9
Tori Sanford <tori.danielle.clark91@gmail.com>
2021-01-06 20:20:27 -0500

    Adding in the prompts for the password generator with the else statements

--- java.js
+++ java.js
@@ -1,16 +1,31 @@
-// Assignment Code
+// This is the generate assigned variable
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
+// These are the passwords and password input responses. 
+function writePassword(create) {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
 
-// Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+function generatePassword() {
+  var passwordLength = prompt("Password must have at least 8 characters. Please tell us how many you would like, greater than 8.");
+  if (passwordLength < 8) {
+    alert("Please choose a password of at least 8 characters. Please start over.");
+    return;
+  }
 
-generateBtn.addEventListener("click", e => { 
-    console.log (e.target)
-})
+  var wantsLowercase = confirm("Would you like lowercase letters? Select ok for yes, cancel for no");
+  if (wantsLowercase) {
+    alert("Lowercase letters included. Select ok to go to the next step");
+  }
+  else {
+    alert("Not including lowercase letters");
+  }
+}
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
